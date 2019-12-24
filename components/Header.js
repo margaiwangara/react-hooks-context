3573f96a06f9b354ba27a08e9692e99aa5d2b592
margaiwangara <margaiwangara@gmail.com>
2019-12-24 15:40:39 +0300

    client side routing added

---
+++ components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Link from "next/link";
+
+import Home from "../components/Home";
+import Contact from "../components/Contact";
+
+export default function Header() {
+  return (
+    <ul>
+      <li>
+        <Link href="/">
+          <a>home</a>
+        </Link>
+      </li>
+
+      <li>
+        <Link href="/contact">
+          <a>contact</a>
+        </Link>
+      </li>
+    </ul>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
+import Link from "next/link";
 
 // components
 import Page from "../components/Page";
-import Data from "../components/Data";
-import About from "../components/About";
-import Home from "../components/Home";
-import Contact from "../components/Contact";
+import Header from "../components/Header";
 
 export default function App({ children }) {
   return (
     <Page>
-      <Router>
-        <div>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-            <li>
-              <Link to="/data">Data</Link>
-            </li>
-          </ul>
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route exact path="/about">
-              <About />
-            </Route>
-            <Route exact path="/contact">
-              <Contact />
-            </Route>
-            <Route exact path="/data">
-              <Data />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+      <Header />
+      <p>Routing in Next.js</p>
     </Page>
   );
 }
